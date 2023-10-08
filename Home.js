28d3c7ba7eeb2376eeac188dbcf1e737c24b01a5
Aashrithaaa <132878455+Aashrithaaa@users.noreply.github.com>
2023-10-08 18:20:13 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import BannerBackground from "../Assets/image1.jpeg";
+import BannerImage from "../Assets/home-banner-image.png";
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Your Favourite Food Delivered Hot & Fresh
+          </h1>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
